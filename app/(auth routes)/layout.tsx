79015a4ee3a163d522e277c81504d28f7ea62be3
Nazar <123.nazar79@gmail.com>
2025-10-18 10:40:40 +0200

    add

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,15 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-
-  //  refresher для auth flow
-  useEffect(() => {
-    router.refresh();
-  }, [router]);
-
   return <>{children}</>;
 }
